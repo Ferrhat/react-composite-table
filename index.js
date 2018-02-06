00925254e4377eddeab76b69779e6f4823475097
NSN-INTRA\csejtei <david.csejtei@nokia.com>
2018-02-06 12:08:29 +0100

    Fixed missing loader type

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-import Table from './src/table';
-
-module.exports = Table;
+module.exports = {
+    entry: './src/table.js',
+    output: {
+        path: __dirname + '/app/assets/javascripts',
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel',
+                exclude: /node_modules/,
+                query: {
+                    cacheDirectory: true,
+                    presets: ['react', 'es2015']
+                }
+            }
+        ]
+    }
+}
