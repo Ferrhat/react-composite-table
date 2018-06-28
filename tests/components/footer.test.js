5d86a78b8a3133823d706afd9661aa34d3acfcad
Csejtei Dávid <david.csejtei@nokia.com>
2018-06-28 10:30:45 +0200

    Fixed test environment

--- tests/components/footer.test.js
+++ tests/components/footer.test.js
@@ -7,6 +7,66 @@ describe('Footer', () => {
     const tableData = [
         {
             id: 1,
+            username: 'Komárom'
+        },
+        {
+            id: 2,
+            username: 'Zalaegerszeg'
+        },
+        {
+            id: 3,
+            username: 'Budapest'
+        }
+    ];
+
+    const numberOfRowsShow = 1;
+    const allPagesNumber = Math.round(tableData.length / numberOfRowsShow);
+    const currentPageNumberChange = jest.fn();
+    const pageSizeChange = jest.fn();
+
+    const footer = mount(
+        <Footer tableData={tableData}
+                onPageSizeChange={pageSizeChange}
+                onCurrentPageNumberChange={currentPageNumberChange}
+                defaultNumberOfRowsShow={numberOfRowsShow}/>
+    );
+
+    it('renders properly', () => {
+        expect(footer).toMatchSnapshot();
+    });
+
+    it('has current page number', () => {
+        expect(footer.state('currentPage')).toEqual(1);
+    });
+
+    it('has total number of pages', () => {
+        const totalNumberOfPages = footer.find('#total-number-of-page');
+        const allPagesNumber = footer.state('allPagesNumber');
+
+        expect(allPagesNumber).toEqual(3);
+    });
+
+    it('has total number of rows', () => {
+        const totalNumberOfRows = footer.state('allPagesNumber');
+
+        expect(totalNumberOfRows).toEqual(3);
+    });
+
+    it('shows total number of rows', () => {
+        const totalNumberOfRowsText = footer.find('#total-rows-number').text();
+
+        expect(totalNumberOfRowsText).toEqual("Total: 3");
+    });
+
+    it('shows current page number', () => {
+        const currentPageNumber = footer.find('#current-page-number').instance().value;
+
+        expect(currentPageNumber).toEqual("1");
+    });
+
+    /*const tableData = [
+        {
+            id: 1,
             username: 'User1'
         },
         {
@@ -53,7 +113,7 @@ describe('Footer', () => {
         wrapper.find('#previous-button').simulate('click');
         expect(spy).toHaveBeenCalledTimes(1);
     });
-
+*/
     /*it('calculates all pages number', () => {
         const spy = jest.spyOn(Footer.prototype, 'componentDidMount');
         const wrapper = mount(
