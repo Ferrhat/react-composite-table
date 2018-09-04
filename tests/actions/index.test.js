ccd367ca6a5857d688b80f8d95903bd04ef5381f
Ferhat Özkan <30701390+Ferrhat@users.noreply.github.com>
2018-09-04 10:33:39 +0200

    Fix filter on multi select fields, Fix render issues on table

--- tests/actions/index.test.js
+++ tests/actions/index.test.js
@@ -2,23 +2,42 @@ import {filterByLabel, filterByMultiSelectLabel, sorting, validateField} from '.
 
 describe('filterByLabel', () => {
     const data = [{'testProperty': 'testValue1'}, {'testProperty': 'testValue2'}];
+    const multiSelectData = [
+        {'testProperty': [{value: 1, label: 'testLabel1'}, {value: 2, label: 'testLabel2'}]},
+        {'testProperty': [{value: 1, label: 'testLabel1'}]},
+    ];
+
     it('returns all rows if the filter is empty', () => {
         const result = filterByLabel(data, 'testProperty', '');
         expect(result).toEqual(data);
     });
 
-    it('returns the filtered rows', () => {
+    it('returns the filtered rows of editable text/select', () => {
         const result = filterByLabel(data, 'testProperty', 'testValue1');
         expect(result).toEqual([{'testProperty': 'testValue1'}]);
+    });
+
+    it('returns the filtered rows of editable multi select', () => {
+        const result = filterByLabel(multiSelectData, 'testProperty', 'testLabel2');
+        expect(result).toEqual([{'testProperty': [{value: 1, label: 'testLabel1'}, {value: 2, label: 'testLabel2'}]}]);
     });
 });
 
 describe('filterByMultiSelectLabel', () => {
     const data = [{'testProperty': 'testValue1'}, {'testProperty': 'testValue2'}];
+    const multiSelectData = [
+        {'testProperty': [{value: 1, label: 'testLabel1'}, {value: 2, label: 'testLabel2'}]},
+        {'testProperty': [{value: 1, label: 'testLabel1'}]},
+    ];
 
-    it('returns the filtered rows', () => {
+    it('returns the filtered rows of editable text/select', () => {
         const result = filterByMultiSelectLabel(data, 'testProperty', [{label: 'testLabel', value: 'testValue1'}]);
         expect(result).toEqual([{'testProperty': 'testValue1'}]);
+    });
+
+    it('returns the filtered rows of editable multi select', () => {
+        const result = filterByMultiSelectLabel(multiSelectData, 'testProperty', [{value: 2, label: 'testLabel2'}]);
+        expect(result).toEqual([{'testProperty': [{value: 1, label: 'testLabel1'}, {value: 2, label: 'testLabel2'}]}]);
     });
 });
 
